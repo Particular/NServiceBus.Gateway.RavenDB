5ba6022f46f91ec651fed82a6fbfb82386c4445f
Andreas Öhlund <andreas.ohlund@particular.net>
2025-11-11 09:55:53 +0100

    Use new dedupe seam

--- src/NServiceBus.Gateway.RavenDB.ClusterWideTx.AcceptanceTests/GatewayTestSuiteConstraints.cs
+++ src/NServiceBus.Gateway.RavenDB.ClusterWideTx.AcceptanceTests/GatewayTestSuiteConstraints.cs
@@ -1,112 +1,105 @@
-﻿namespace NServiceBus.Gateway.AcceptanceTests
+﻿namespace NServiceBus.Gateway.AcceptanceTests;
+
+using NServiceBus.AcceptanceTesting.Support;
+using Raven.Client.Documents;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+
+public partial class GatewayTestSuiteConstraints
 {
-    using NServiceBus.AcceptanceTesting.Support;
-    using NServiceBus.Configuration.AdvancedExtensibility;
-    using Raven.Client.Documents;
-    using Raven.Client.ServerWide;
-    using Raven.Client.ServerWide.Operations;
-    using System;
-    using System.Threading.Tasks;
-    using System.Collections.Generic;
-    using System.Threading;
-
-    public partial class GatewayTestSuiteConstraints
+    public GatewaySettings ConfigureGateway(string endpointName, EndpointConfiguration configuration, RunSettings settings)
     {
-        public Task<GatewayDeduplicationConfiguration> ConfigureDeduplicationStorage(string endpointName, EndpointConfiguration configuration, RunSettings settings)
+        var ravenGatewayDeduplicationConfiguration = new RavenGatewayDeduplicationConfiguration((_, _) =>
         {
-            var ravenGatewayDeduplicationConfiguration = new RavenGatewayDeduplicationConfiguration((builder, _) =>
-            {
-                var databaseName = Guid.NewGuid().ToString();
-                var documentStore = GetInitializedDocumentStore(databaseName);
+            var databaseName = Guid.NewGuid().ToString();
+            var documentStore = GetInitializedDocumentStore(databaseName);
 
-                documentStore.Maintenance.Server.Send(new CreateDatabaseOperation(new DatabaseRecord(databaseName)));
+            documentStore.Maintenance.Server.Send(new CreateDatabaseOperation(new DatabaseRecord(databaseName)));
 
-                try
-                {
-                    semaphoreSlim.Wait();
-                    databases.Add(databaseName);
-                }
-                finally
-                {
-                    semaphoreSlim.Release();
-                }
-
-                return documentStore;
-            })
-            {
-                EnableClusterWideTransactions = true
-            };
-
-            var gatewaySettings = configuration.Gateway(ravenGatewayDeduplicationConfiguration);
-            configuration.GetSettings().Set(gatewaySettings);
-
-            return Task.FromResult<GatewayDeduplicationConfiguration>(ravenGatewayDeduplicationConfiguration);
-        }
-
-        public async Task Cleanup()
-        {
-            await semaphoreSlim.WaitAsync();
             try
             {
-                foreach (var databaseName in databases)
-                {
-                    // Periodically the delete will throw an exception because Raven has the database locked
-                    // To solve this we have a retry loop with a delay
-                    var triesLeft = 3;
-
-                    while (triesLeft-- > 0)
-                    {
-                        try
-                        {
-                            // We are using a new store because the global one is disposed of before cleanup
-                            using (var storeForDeletion = GetInitializedDocumentStore(databaseName))
-                            {
-                                storeForDeletion.Maintenance.Server.Send(new DeleteDatabasesOperation(storeForDeletion.Database, hardDelete: true));
-                                break;
-                            }
-                        }
-                        catch
-                        {
-                            if (triesLeft == 0)
-                            {
-                                throw;
-                            }
-
-                            await Task.Delay(250);
-                        }
-                    }
-
-                    Console.WriteLine("Deleted '{0}' database", databaseName);
-                }
-
-                databases.Clear();
+                semaphoreSlim.Wait();
+                databases.Add(databaseName);
             }
             finally
             {
                 semaphoreSlim.Release();
             }
-        }
-
-        static DocumentStore GetInitializedDocumentStore(string defaultDatabase)
-        {
-            var urls = Environment.GetEnvironmentVariable("CommaSeparatedRavenClusterUrls");
-            if (urls == null)
-            {
-                throw new Exception("RavenDB cluster URLs must be specified in an environment variable named CommaSeparatedRavenClusterUrls.");
-            }
-
-            var documentStore = new DocumentStore
-            {
-                Urls = urls.Split(','),
-                Database = defaultDatabase
-            };
-
-            documentStore.Initialize();
 
             return documentStore;
+        })
+        {
+            EnableClusterWideTransactions = true
+        };
+
+        return configuration.Gateway(ravenGatewayDeduplicationConfiguration);
+    }
+
+    public async Task Cleanup()
+    {
+        await semaphoreSlim.WaitAsync();
+        try
+        {
+            foreach (var databaseName in databases)
+            {
+                // Periodically the delete will throw an exception because Raven has the database locked
+                // To solve this we have a retry loop with a delay
+                var triesLeft = 3;
+
+                while (triesLeft-- > 0)
+                {
+                    try
+                    {
+                        // We are using a new store because the global one is disposed of before cleanup
+                        using var storeForDeletion = GetInitializedDocumentStore(databaseName);
+                        storeForDeletion.Maintenance.Server.Send(new DeleteDatabasesOperation(storeForDeletion.Database, hardDelete: true));
+                        break;
+                    }
+                    catch
+                    {
+                        if (triesLeft == 0)
+                        {
+                            throw;
+                        }
+
+                        await Task.Delay(250);
+                    }
+                }
+
+                Console.WriteLine("Deleted '{0}' database", databaseName);
+            }
+
+            databases.Clear();
+        }
+        finally
+        {
+            semaphoreSlim.Release();
+        }
+    }
+
+    static DocumentStore GetInitializedDocumentStore(string defaultDatabase)
+    {
+        var urls = Environment.GetEnvironmentVariable("CommaSeparatedRavenClusterUrls");
+        if (urls == null)
+        {
+            throw new Exception("RavenDB cluster URLs must be specified in an environment variable named CommaSeparatedRavenClusterUrls.");
         }
 
-        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
-        List<string> databases = [];
+        var documentStore = new DocumentStore
+        {
+            Urls = urls.Split(','),
+            Database = defaultDatabase
+        };
+
+        documentStore.Initialize();
+
+        return documentStore;
     }
+
+    static readonly SemaphoreSlim semaphoreSlim = new(1, 1);
+    readonly List<string> databases = [];
 }
--- src/NServiceBus.Gateway.RavenDB/RavenGatewayDeduplicationConfiguration.cs
+++ src/NServiceBus.Gateway.RavenDB/RavenGatewayDeduplicationConfiguration.cs
@@ -7,6 +7,8 @@
     using Raven.Client.ServerWide.Commands;
     using Settings;
     using System;
+    using Features;
+    using Microsoft.Extensions.DependencyInjection;
     using Raven.Client.ServerWide.Operations;
 
     /// <summary>
@@ -22,62 +24,6 @@
             ArgumentNullException.ThrowIfNull(documentStoreFactory);
 
             this.documentStoreFactory = documentStoreFactory;
-        }
-
-        /// <inheritdoc />
-        public override void Setup(IReadOnlySettings settings)
-        {
-            this.settings = settings;
-
-            base.Setup(settings);
-        }
-
-        /// <inheritdoc />
-        public override IGatewayDeduplicationStorage CreateStorage(IServiceProvider builder)
-        {
-            var documentStore = documentStoreFactory(builder, settings);
-
-            EnsureCompatibleServerVersion(documentStore);
-            EnsureClusterConfiguration(documentStore);
-            EnableExpirationFeature(documentStore, FrequencyToRunDeduplicationDataCleanup);
-
-            return new RavenGatewayDeduplicationStorage(documentStore, DeduplicationDataTimeToLive, EnableClusterWideTransactions);
-        }
-
-        static void EnableExpirationFeature(IDocumentStore documentStore, long frequencyToRunDeduplicationDataCleanup)
-        {
-            documentStore.Maintenance.Send(new ConfigureExpirationOperation(new ExpirationConfiguration
-            {
-                Disabled = false,
-                DeleteFrequencyInSec = frequencyToRunDeduplicationDataCleanup
-            }));
-        }
-
-        void EnsureClusterConfiguration(IDocumentStore store)
-        {
-            using (var s = store.OpenSession())
-            {
-                var getTopologyCmd = new GetDatabaseTopologyCommand();
-                s.Advanced.RequestExecutor.Execute(getTopologyCmd, s.Advanced.Context);
-
-                if (getTopologyCmd.Result.Nodes.Count > 1 && !EnableClusterWideTransactions)
-                {
-                    throw new InvalidOperationException("The RavenDB cluster contains multiple nodes. To safely operate in multi-node environments, enable cluster-wide transactions.");
-                }
-            }
-        }
-
-        void EnsureCompatibleServerVersion(IDocumentStore documentStore)
-        {
-            var requiredVersion = new Version(5, 2);
-            var serverVersion = documentStore.Maintenance.Server.Send(new GetBuildNumberOperation());
-            var fullVersion = new Version(serverVersion.FullVersion);
-
-            if (fullVersion.Major < requiredVersion.Major ||
-                (fullVersion.Major == requiredVersion.Major && fullVersion.Minor < requiredVersion.Minor))
-            {
-                throw new Exception($"We detected that the server is running on version {serverVersion.FullVersion}. RavenDB persistence requires RavenDB server 5.2 or higher");
-            }
         }
 
         /// <summary>
@@ -96,7 +42,62 @@
         /// </summary>
         public bool EnableClusterWideTransactions { get; set; }
 
-        IReadOnlySettings settings;
+        /// <inheritdoc />
+        protected override void EnableFeature(SettingsHolder settings) => settings.EnableFeature<RavenGatewayDeduplication>();
+
         readonly Func<IServiceProvider, IReadOnlySettings, IDocumentStore> documentStoreFactory;
+
+        class RavenGatewayDeduplication : Feature
+        {
+            protected override void Setup(FeatureConfigurationContext context)
+            {
+                var configuration = context.Settings.Get<RavenGatewayDeduplicationConfiguration>();
+
+                context.Services.AddSingleton<IGatewayDeduplicationStorage>(sp =>
+                {
+                    var documentStore = configuration.documentStoreFactory(sp, sp.GetRequiredService<IReadOnlySettings>());
+
+                    EnsureCompatibleServerVersion(documentStore);
+                    EnsureClusterConfiguration(documentStore, configuration.EnableClusterWideTransactions);
+                    EnableExpirationFeature(documentStore, configuration.FrequencyToRunDeduplicationDataCleanup);
+
+                    return new RavenGatewayDeduplicationStorage(documentStore, configuration.DeduplicationDataTimeToLive, configuration.EnableClusterWideTransactions);
+                });
+            }
+
+            static void EnableExpirationFeature(IDocumentStore documentStore, long frequencyToRunDeduplicationDataCleanup)
+            {
+                documentStore.Maintenance.Send(new ConfigureExpirationOperation(new ExpirationConfiguration
+                {
+                    Disabled = false,
+                    DeleteFrequencyInSec = frequencyToRunDeduplicationDataCleanup
+                }));
+            }
+
+            static void EnsureClusterConfiguration(IDocumentStore store, bool clusterWideTransactionsEnabled)
+            {
+                using var s = store.OpenSession();
+                var getTopologyCmd = new GetDatabaseTopologyCommand();
+                s.Advanced.RequestExecutor.Execute(getTopologyCmd, s.Advanced.Context);
+
+                if (getTopologyCmd.Result.Nodes.Count > 1 && !clusterWideTransactionsEnabled)
+                {
+                    throw new InvalidOperationException("The RavenDB cluster contains multiple nodes. To safely operate in multi-node environments, enable cluster-wide transactions.");
+                }
+            }
+
+            static void EnsureCompatibleServerVersion(IDocumentStore documentStore)
+            {
+                var requiredVersion = new Version(5, 2);
+                var serverVersion = documentStore.Maintenance.Server.Send(new GetBuildNumberOperation());
+                var fullVersion = new Version(serverVersion.FullVersion);
+
+                if (fullVersion.Major < requiredVersion.Major ||
+                    (fullVersion.Major == requiredVersion.Major && fullVersion.Minor < requiredVersion.Minor))
+                {
+                    throw new Exception($"We detected that the server is running on version {serverVersion.FullVersion}. RavenDB persistence requires RavenDB server 5.2 or higher");
+                }
+            }
+        }
     }
 }
